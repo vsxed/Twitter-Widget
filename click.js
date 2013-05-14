1b485fecdd731440e4423f2cfbdfd70b74f7405a
Eduard Mayer <kontakt@eduardmayer.de>
2013-05-14 15:30:37 +0200

    Added a lot of shit, man

---
+++ click.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+
+		$('.animation').click(function () {
+			$('.back').addClass('reverse');
+		});
+
+		$('.map').click(function () {
+			$('.back').addClass('animation');
+		});
+
+
+});
